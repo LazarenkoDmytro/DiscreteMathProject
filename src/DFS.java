885e7690ffc18ed473d8666dbeb05fdda3d66850
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-04-19 01:29:17 +0300

    DFS.java JavaDocs

--- src/DFS.java
+++ src/DFS.java
@@ -1,4 +1,16 @@
+/**
+ * Provides methods to perform Depth-First Search (DFS) on a graph.
+ */
 public class DFS {
+    /**
+     * Performs DFS on a graph using adjacency lists, updating the reachability matrix.
+     *
+     * @param graph              The graph to be searched
+     * @param start              The start vertex for DFS
+     * @param vertex             The current vertex being visited
+     * @param visited            Array to keep track of visited vertices
+     * @param reachabilityMatrix Matrix to update the reachability from the start vertex
+     */
     private static void dfsUsingAdjacencyLists(Graph graph, int start, int vertex, boolean[] visited, int[][] reachabilityMatrix) {
         visited[vertex] = true;
         reachabilityMatrix[start][vertex] = 1;
@@ -9,6 +21,12 @@ public class DFS {
         }
     }
 
+    /**
+     * Computes the reachability matrix using adjacency lists.
+     *
+     * @param graph The graph for which the matrix is computed
+     * @return The reachability matrix
+     */
     public static int[][] getReachabilityMatrixUsingAdjacencyLists(Graph graph) {
         int[][] reachabilityMatrix = new int[graph.getVerticesNumber()][graph.getVerticesNumber()];
         for (int i = 0; i < graph.getVerticesNumber(); i++) {
@@ -19,6 +37,15 @@ public class DFS {
         return reachabilityMatrix;
     }
 
+    /**
+     * Performs DFS on a graph using adjacency matrix, updating the reachability matrix.
+     *
+     * @param graph              The graph to be searched
+     * @param start              The start vertex for DFS
+     * @param vertex             The current vertex being visited
+     * @param visited            Array to keep track of visited vertices
+     * @param reachabilityMatrix Matrix to update the reachability from the start vertex
+     */
     private static void dfsUsingAdjacencyMatrix(Graph graph, int start, int vertex, boolean[] visited, int[][] reachabilityMatrix) {
         visited[vertex] = true;
         reachabilityMatrix[start][vertex] = 1;
@@ -29,6 +56,12 @@ public class DFS {
         }
     }
 
+    /**
+     * Computes the reachability matrix using an adjacency matrix.
+     *
+     * @param graph The graph for which the matrix is computed
+     * @return The reachability matrix
+     */
     public static int[][] getReachabilityMatrixUsingAdjacencyMatrix(Graph graph) {
         int[][] reachabilityMatrix = new int[graph.getVerticesNumber()][graph.getVerticesNumber()];
         for (int i = 0; i < graph.getVerticesNumber(); i++) {
