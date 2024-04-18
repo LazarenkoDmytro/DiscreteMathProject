7068453b1b5996fc165069c5404595ec1eb0566a
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-04-19 00:24:20 +0300

    Rebuild DFS and BFS to be static

--- src/DFS.java
+++ src/DFS.java
@@ -1,45 +1,39 @@
 public class DFS {
-    private final Graph graph;
-
-    public DFS(Graph graph) {
-        this.graph = graph;
-    }
-
-    private void dfsUsingAdjacencyLists(int start, int vertex, boolean[] visited, int[][] reachabilityMatrix) {
+    private static void dfsUsingAdjacencyLists(Graph graph, int start, int vertex, boolean[] visited, int[][] reachabilityMatrix) {
         visited[vertex] = true;
         reachabilityMatrix[start][vertex] = 1;
         for (int adjVertex : graph.getAdjacencyLists().get(vertex)) {
             if (!visited[adjVertex]) {
-                dfsUsingAdjacencyLists(start, adjVertex, visited, reachabilityMatrix);
+                dfsUsingAdjacencyLists(graph, start, adjVertex, visited, reachabilityMatrix);
             }
         }
     }
 
-    public int[][] getReachabilityMatrixUsingAdjacencyLists() {
+    public static int[][] getReachabilityMatrixUsingAdjacencyLists(Graph graph) {
         int[][] reachabilityMatrix = new int[graph.getVerticesNumber()][graph.getVerticesNumber()];
         for (int i = 0; i < graph.getVerticesNumber(); i++) {
             boolean[] visited = new boolean[graph.getVerticesNumber()];
-            dfsUsingAdjacencyLists(i, i, visited, reachabilityMatrix);
+            dfsUsingAdjacencyLists(graph, i, i, visited, reachabilityMatrix);
         }
 
         return reachabilityMatrix;
     }
 
-    private void dfsUsingAdjacencyMatrix(int start, int vertex, boolean[] visited, int[][] reachabilityMatrix) {
+    private static void dfsUsingAdjacencyMatrix(Graph graph, int start, int vertex, boolean[] visited, int[][] reachabilityMatrix) {
         visited[vertex] = true;
         reachabilityMatrix[start][vertex] = 1;
         for (int i = 0; i < graph.getVerticesNumber(); i++) {
             if (graph.getAdjacencyMatrix()[vertex][i] == 1 && !visited[i]) {
-                dfsUsingAdjacencyMatrix(start, i, visited, reachabilityMatrix);
+                dfsUsingAdjacencyMatrix(graph, start, i, visited, reachabilityMatrix);
             }
         }
     }
 
-    public int[][] getReachabilityMatrixUsingAdjacencyMatrix() {
+    public static int[][] getReachabilityMatrixUsingAdjacencyMatrix(Graph graph) {
         int[][] reachabilityMatrix = new int[graph.getVerticesNumber()][graph.getVerticesNumber()];
         for (int i = 0; i < graph.getVerticesNumber(); i++) {
             boolean[] visited = new boolean[graph.getVerticesNumber()];
-            dfsUsingAdjacencyMatrix(i, i, visited, reachabilityMatrix);
+            dfsUsingAdjacencyMatrix(graph, i, i, visited, reachabilityMatrix);
         }
 
         return reachabilityMatrix;
